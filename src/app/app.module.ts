79365441e971d6b873ca19ec87e03c5e4c1f6950
josseline534 <joseline-mls@hotmail.es>
2021-01-29 17:35:48 -0600

    Creacion de componentes y Maquetacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MiComponente } from './components/miComponente/miComponente.component';
-import { PruebasComponent } from './components/pruebas/pruebas.component'
+import { PruebasComponent } from './components/pruebas/pruebas.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ArticleComponent } from './components/article/article.component';
+import { FooterComponent } from './components/footer/footer.component'
 @NgModule({
   declarations: [
     AppComponent,
     MiComponente,
-    PruebasComponent
+    PruebasComponent,
+    HeaderComponent,
+    SliderComponent,
+    SidebarComponent,
+    ArticleComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
