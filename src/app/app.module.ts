58ebf369c16846df0694db8b50352e413a9a8ee2
josseline534 <joseline-mls@hotmail.es>
2021-01-29 15:48:22 -0600

    Estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MiComponente } from './components/miComponente/miComponente.component'
+import { MiComponente } from './components/miComponente/miComponente.component';
+import { PruebasComponent } from './components/pruebas/pruebas.component'
 @NgModule({
   declarations: [
     AppComponent,
-    MiComponente
+    MiComponente,
+    PruebasComponent
   ],
   imports: [
     BrowserModule
