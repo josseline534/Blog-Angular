ba6f8f5887f182be1923cdb339d33e12a983acb0
josseline534 <joseline-mls@hotmail.es>
2021-01-29 13:48:57 -0600

    Mi primer componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { MiComponente } from './components/miComponente/miComponente.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MiComponente
   ],
   imports: [
     BrowserModule
