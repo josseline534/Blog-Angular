4f36380b91f3bd9977dba88f3961b06ded754591
josseline534 <joseline-mls@hotmail.es>
2021-01-29 18:33:02 -0600

    Creacion de rutas y solucion de ModuleWithProviders<any>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { routing, appRoutingProviders } from './app.routing'
 
 import { AppComponent } from './app.component';
 import { MiComponente } from './components/miComponente/miComponente.component';
@@ -8,7 +9,11 @@ import { HeaderComponent } from './components/header/header.component';
 import { SliderComponent } from './components/slider/slider.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { ArticleComponent } from './components/article/article.component';
-import { FooterComponent } from './components/footer/footer.component'
+import { FooterComponent } from './components/footer/footer.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+import { PaginaComponent } from './components/pagina/pagina.component';
+import { PeliculasComponent } from './components/peliculas/peliculas.component'
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,12 +23,17 @@ import { FooterComponent } from './components/footer/footer.component'
     SliderComponent,
     SidebarComponent,
     ArticleComponent,
-    FooterComponent
+    FooterComponent,
+    BlogComponent,
+    FormularioComponent,
+    PaginaComponent,
+    PeliculasComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing //modulo
   ],
-  providers: [],
+  providers: [appRoutingProviders], //servicio
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,44 @@
+//Importar los modulos del router de angular
+import { ModuleWithProviders } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router'
+import { from } from 'rxjs'
+
+//Importar componentes a los que se le va hacer una pagina exclusiva
+import { ArticleComponent } from './components/article/article.component'
+import { BlogComponent } from './components/blog/blog.component'
+import { FormularioComponent } from './components/formulario/formulario.component'
+import { PeliculasComponent } from './components/peliculas/peliculas.component'
+import { PaginaComponent } from './components/pagina/pagina.component'
+
+//Array de rutas
+const appRoutes: Routes = [
+    {
+        path: '',
+        component: ArticleComponent
+    },
+    {
+        path: 'home',
+        component: ArticleComponent
+    },
+    {
+        path: 'blog',
+        component: BlogComponent
+    },
+    {
+        path: 'formulario',
+        component: FormularioComponent
+    },
+    {
+        path: 'peliculas',
+        component: PeliculasComponent
+    },
+    {
+        path: 'pagina-de-pruebas',
+        component: PaginaComponent
+    }
+]
+
+//Exportar el modulo de rutas
+export const appRoutingProviders: any[] = []
+//Carga la configuracion de rutas
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(appRoutes)
